1b420d3d2b09964d52e56de23a70bcca5467b52a
wKich <wKich@users.noreply.github.com>
2014-02-26 10:59:41 +0600

    Improve saving.

--- datafile.cpp
+++ datafile.cpp
@@ -13,31 +13,20 @@ QString DataFile::getFileName() const
 void DataFile::initialize(const double &dt, const double &time, const QVector2D &min, const QVector2D &max,
                           const QList<StaticPlanet> &sPlanets, const QList<DynamicPlanet> &dPlanets)
 {
-    float x,y;
     m_file.setFileName(QDateTime::currentDateTime().toString("yyyy_MM_dd_hh_mm_ss") + "_points.dat");
     m_file.open(QFile::ReadWrite);
     m_file.write(reinterpret_cast<const char*>(&dt), sizeof(double));
     m_file.write(reinterpret_cast<const char*>(&time), sizeof(double));
-    x = min.x();
-    y = min.y();
-    m_file.write(reinterpret_cast<char*>(&x), sizeof(float));
-    m_file.write(reinterpret_cast<char*>(&y), sizeof(float));
-    x = max.x();
-    y = max.y();
-    m_file.write(reinterpret_cast<char*>(&x), sizeof(float));
-    m_file.write(reinterpret_cast<char*>(&y), sizeof(float));
+    m_file.write(reinterpret_cast<const char*>(&min), sizeof(QVector2D));
+    m_file.write(reinterpret_cast<const char*>(&max), sizeof(QVector2D));
     int sPlanetsSize = sPlanets.size();
     m_file.write(reinterpret_cast<char*>(&sPlanetsSize), sizeof(int));
     for (int i = 0; i < sPlanetsSize; i++) {
         QRgb color = sPlanets.at(i).color.rgb();
         m_file.write(reinterpret_cast<char*>(&color), sizeof(QRgb));
     }
-    for (int i = 0; i < sPlanetsSize; i++) {
-        x = sPlanets.at(i).startPosition.x();
-        y = sPlanets.at(i).startPosition.y();
-        m_file.write(reinterpret_cast<char*>(&x), sizeof(float));
-        m_file.write(reinterpret_cast<char*>(&y), sizeof(float));
-    }
+    for (int i = 0; i < sPlanetsSize; i++)
+        m_file.write(reinterpret_cast<const char*>(&sPlanets.at(i).startPosition), sizeof(QVector2D));
     int dPlanetsSize = dPlanets.size();
     m_file.write(reinterpret_cast<char*>(&dPlanetsSize), sizeof(int));
     for (int i = 0; i < dPlanetsSize; i++) {
